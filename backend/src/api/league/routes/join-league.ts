2f467ab98848794fe66131e85f39b6c6d1190e52
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-04 19:04:18 +0100

    Changes

---
+++ backend/src/api/league/routes/join-league.ts
@@ -0,0 +1,14 @@
+export default {
+    routes: [
+      {
+        method: 'POST',
+        path: '/leagues/:id/join',
+        handler: 'league.joinLeague',
+        config: {
+          auth: { scope: ['authenticated'] },
+        },
+        type: 'content-api',
+      },
+    ],
+  };
+  
