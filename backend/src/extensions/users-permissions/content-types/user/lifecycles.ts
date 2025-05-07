304712493a64354b88b25a39759e1c1d22d9313a
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-07 17:45:15 +0100

    autoplayer

--- backend/src/extensions/users-permissions/content-types/user/lifecycles.ts
+++ backend/src/extensions/users-permissions/content-types/user/lifecycles.ts
@@ -1,21 +1,13 @@
 export default {
-    async afterCreate(event, { strapi }) {
+    async afterCreate(event) {
       const { result } = event;
-      if (!result || !result.id) return;
   
-      const existingPlayer = await strapi.entityService.findMany('api::player.player', {
-        filters: { user: { id: result.id } },
-      });
-  
-      if (existingPlayer?.length > 0) return;
-  
+      // Create and link a Player entry to this User
       await strapi.entityService.create('api::player.player', {
         data: {
-          name: result.username || 'Unnamed Player',
+          name: result.username,
           email: result.email,
           user: result.id,
-          ranking: 0,
-          faction: 'Unknown',
         },
       });
     },
