5e58971737b59d4abef2ca66c36943faadb92514
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-08 19:08:48 +0100

    fixes

--- backend/src/api/league-player/routes/league-player.ts
+++ backend/src/api/league-player/routes/league-player.ts
@@ -1,7 +1,27 @@
-/**
- * league-player router
- */
+// path: src/api/league-player/routes/league-player.ts
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::league-player.league-player');
+const customRouterConfig = {
+  config: {
+    find: {},
+    findOne: {},
+    create: {},
+    update: {},
+    delete: {},
+  },
+ 
+  routes: [
+    {
+      method: 'GET',
+      path: '/league-players/:id/matches',
+      handler: 'league-player.matches',
+      config: {
+        auth: true,
+        policies: [],
+      },
+    },
+  ],
+} as any;
+
+export default factories.createCoreRouter('api::league-player.league-player', customRouterConfig);
