8fe739e614704339f3c0fc60b7cc695f2e1e07df
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-09 02:06:16 +0100

    adding defaults

--- backend/src/api/player/routes/player.ts
+++ backend/src/api/player/routes/player.ts
@@ -1,17 +1,26 @@
+// path: src/api/player/routes/player.ts
+
 import { factories } from '@strapi/strapi';
 
-const customRoutes = [
-  {
-    method: 'GET',
-    path: '/me/player',
-    handler: 'player.me',
-    config: {
-      auth: false, // make true when done testing
-    },
+const customRouterConfig = {
+  config: {
+    find: {},
+    findOne: {},
+    create: {},
+    update: {},
+    delete: {},
   },
-];
+  routes: [
+    {
+      method: 'GET',
+      path: '/me/player',
+      handler: 'player.me',
+      config: {
+        auth: false, // set to true once working
+        policies: [],
+      },
+    },
+  ],
+} as any;
 
-export default factories.createCoreRouter('api::player.player', {
-  config: {},
-  routes: customRoutes,
-} as any);
+export default factories.createCoreRouter('api::player.player', customRouterConfig);
