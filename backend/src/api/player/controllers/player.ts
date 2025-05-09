e7f13c0309e2642111753b50ff3391b4563cc6e3
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-09 01:08:01 +0100

    controller fix

--- backend/src/api/player/controllers/player.ts
+++ backend/src/api/player/controllers/player.ts
@@ -1,26 +1,27 @@
-export default {
-    async me(ctx) {
-      const userId = ctx.state.user?.id;
-  
-      if (!userId) {
-        return ctx.unauthorized("No authenticated user");
-      }
-  
-      const player = await strapi.entityService.findMany("api::player.player", {
-        filters: {
-          user: userId,
-        },
-        populate: ['user'],
-      });
-  
-      if (!player || player.length === 0) {
-        return ctx.notFound("No player associated with this user");
-      }
-  
-      ctx.body = {
-        id: player[0].id,
-        ...player[0],
-      };
-    },
-  };
-  
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::player.player', ({ strapi }) => ({
+  async me(ctx) {
+    const userId = ctx.state.user?.id;
+
+    if (!userId) {
+      return ctx.unauthorized("No authenticated user");
+    }
+
+    const player = await strapi.entityService.findMany("api::player.player", {
+      filters: {
+        user: userId,
+      },
+      populate: ['user'],
+    });
+
+    if (!player || player.length === 0) {
+      return ctx.notFound("No player associated with this user");
+    }
+
+    ctx.body = {
+      id: player[0].id,
+      ...player[0],
+    };
+  },
+}));
