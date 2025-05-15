41d96ce765814c0387688d0af713fe140537150d
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-15 15:39:37 +0100

    index not league-player.ts

---
+++ backend/src/api/league-player/routes/index.ts
@@ -0,0 +1,58 @@
+export default {
+  routes: [
+    {
+      method: 'GET',
+      path: '/league-players',
+      handler: 'league-player.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/league-players/:id',
+      handler: 'league-player.findOne',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'POST',
+      path: '/league-players',
+      handler: 'league-player.create',
+      config: {
+        auth: {
+          required: true,
+        },
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/league-players/:id',
+      handler: 'league-player.update',
+      config: {
+        auth: {
+          required: true,
+        },
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/league-players/:id',
+      handler: 'league-player.delete',
+      config: {
+        auth: {
+          required: true,
+        },
+      },
+    },
+    {
+      method: 'GET',
+      path: '/league-players/:id/matches',
+      handler: 'league-player.matches',
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
