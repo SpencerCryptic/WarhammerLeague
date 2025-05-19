873f58297c3399d2f5ead724579540d39af8f10d
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-05-20 00:42:09 +0100

    minor change

--- backend/src/api/match/routes/match.ts
+++ backend/src/api/match/routes/match.ts
@@ -1,54 +1,48 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::match.match', {
-  config: {
-    find: {},
-    findOne: {},
-    create: {},
-    update: {
-        auth: {
-          scope: ['plugin::users-permissions.user'],
-        },
-      },
-    delete: {},
-    report: {
-        auth: {
-          scope: ['plugin::users-permissions.user'],
-        },
-      },
-    respondToProposal: {
-        auth: {
-          scope: ['plugin::users-permissions.user'],
-        },
-      },
-  },
-  routes: [
-    {
-      method: 'POST',
-      path: '/matches/:id/report',
-      handler: 'match.report',
-      config: {
-        auth: true,
-        policies: [],
-      },
+    config: {
+      find: {},
+      findOne: {},
+      create: {},
+      delete: {},
+      report: { auth: true },
+      respondToProposal: { auth: true },
     },
-      {
-        method: 'POST',
-        path: '/matches/:id/respond-proposal',
-        handler: 'match.respondToProposal',
-        config: {
-            auth: true,
-            policies: [],
-          },
-      },
+    routes: [
       {
         method: 'PUT',
         path: '/matches/:id',
         handler: 'match.update',
         config: {
-          auth: true,
+          auth: {
+            scope: ['plugin::users-permissions.user'],
+          },
           policies: [],
         },
-      }      
-  ],
-} as any);
+      },
+      {
+        method: 'POST',
+        path: '/matches/:id/report',
+        handler: 'match.report',
+        config: {
+          auth: {
+            scope: ['plugin::users-permissions.user'],
+          },
+          policies: [],
+        },
+      },
+      {
+        method: 'POST',
+        path: '/matches/:id/respond-proposal',
+        handler: 'match.respondToProposal',
+        config: {
+          auth: {
+            scope: ['plugin::users-permissions.user'],
+          },
+          policies: [],
+        },
+      },
+    ],
+  } as any);
+  
