5c2527830ffdab588122f91d553ba531e9ee5bee
TheCptnnn <109996802+TheCptnnn@users.noreply.github.com>
2025-07-10 16:36:42 +0100

    RTD

--- frontend/src/app/leagues/[documentId]/page.tsx
+++ frontend/src/app/leagues/[documentId]/page.tsx
@@ -1,6 +1,8 @@
 import Tabgroup from '@/components/Tabgroup';
 import { error } from 'console';
 import React from 'react'
+import { RichText } from "@graphcms/rich-text-react-renderer";
+
 
 const getLeague = async (documentId: string) => {
   const response = await fetch(`http://localhost:1337/api/leagues/${documentId}`);
@@ -20,13 +22,21 @@ const League = async ({ params }: { params: any }) => {
   return (
     <div>
       <h1 className='mb-20 text-6xl'>
-        { league.data.name }
+        {league.data.name}
       </h1>
+  
       <div className="block max-w-6xl m-4 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <Tabgroup />
       </div>
+  
+      {league.data?.description && (
+        <div className="max-w-6xl m-4 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+          <RichText content={league.data.description} />
+        </div>
+      )}
     </div>
-  )
-}
+  )  
+  
+}  
 
 export default League
