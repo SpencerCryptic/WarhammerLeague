d760c7cf5860f95fa4765a958b8b3abda316693e
Stanley Peddle <stanley.peddle@smarterdm.com>
2025-08-27 15:34:11 +0100

    re-added matches dashboard

--- frontend/src/components/MatchesDashboard.tsx
+++ frontend/src/components/MatchesDashboard.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { LeaguePlayer } from './TableRow';
@@ -48,8 +49,8 @@ const MatchesDashboard = () => {
   }
   
   const pathName: string[] = usePathname().split('/')
-  const documentId = pathName[2]
-  getLeague(documentId)
+  const leagueDocumentId = pathName[2]
+  getLeague(leagueDocumentId)
 
   if (league && league.statusleague.toString() === 'planned') {
     return <div>
@@ -61,26 +62,28 @@ const MatchesDashboard = () => {
     <div>
       {matches.map((match: Match) => (
         <div key={match.documentId} className="m-4 p-2 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 transition-all duration-150">
-          <ul>
-            <li className='pb-4'>
-              <p className='text-sm text-gray-400 capitalize'>{match.statusMatch}</p>
-              <p>{match.proposalTimestamp ? match.proposalTimestamp.toDateString() : ''}</p>
-            </li>
-            <li className='mx-4 pb-4'>
-              <div className='flex'>
-                <p className='capitalize'>{match.leaguePlayer1.leagueName}</p>
-                <p className={match.leaguePlayer1Result === 2 ? 'font-bold' : '' + 'ml-auto'}>{match.statusMatch.toString() === 'played' ? match.leaguePlayer1Score : ''}</p>              
-              </div>
-              <p className='text-xs text-gray-400'>{match.leaguePlayer1.faction}</p>
-            </li>
-            <li className='mx-4 pb-4'>
-              <div className='flex'>
-                <p className='capitalize'>{match.leaguePlayer2.leagueName}</p>
-                <p className={match.leaguePlayer2Result === 2 ? 'font-bold' : ''}>{match.statusMatch.toString() === 'played' ? match.leaguePlayer2Score: ''}</p>
-              </div>
-              <p className='text-xs text-gray-400'>{match.leaguePlayer2.faction}</p>
-            </li>
-          </ul>
+          <Link href={'/leagues/' + leagueDocumentId + '/matches/' + match.documentId}>
+            <ul>
+              <li className='pb-4'>
+                <p className='text-sm text-gray-400 capitalize'>{match.statusMatch}</p>
+                <p>{match.proposalTimestamp ? match.proposalTimestamp.toDateString() : ''}</p>
+              </li>
+              <li className='mx-4 pb-4'>
+                <div className='flex'>
+                  <p className='capitalize'>{match.leaguePlayer1.leagueName}</p>
+                  <p className={match.leaguePlayer1Result === 2 ? 'font-bold' : '' + 'ml-auto'}>{match.statusMatch.toString() === 'played' ? match.leaguePlayer1Score : ''}</p>              
+                </div>
+                <p className='text-xs text-gray-400'>{match.leaguePlayer1.faction}</p>
+              </li>
+              <li className='mx-4 pb-4'>
+                <div className='flex'>
+                  <p className='capitalize'>{match.leaguePlayer2.leagueName}</p>
+                  <p className={match.leaguePlayer2Result === 2 ? 'font-bold' : ''}>{match.statusMatch.toString() === 'played' ? match.leaguePlayer2Score: ''}</p>
+                </div>
+                <p className='text-xs text-gray-400'>{match.leaguePlayer2.faction}</p>
+              </li>
+            </ul>
+          </Link>
         </div>
       ))}
     </div>
